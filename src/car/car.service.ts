0a6b996971d5c410b4efcfdce825c7daad0d72d7
Omar Jabour <jabour.omar@hotmail.com>
2026-04-17 18:01:57 +0200

    :truck: Nest .  create mock.ts for cars .routes   :truck:

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -3,7 +3,13 @@ import { CARS } from './cars.mock';
 
 @Injectable()
 export class CarService {
+    private cars = CARS;
     public async getCars() {
-        return {};
+        return this.cars;
     }
+
+    public async getCarById(id: number) {}
+    public async postCar(car: any) {}
+    public async deleteCar(id: number) {}
+    public async updateCar(id: number, car: any) {}
 }
