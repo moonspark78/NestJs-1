bed09cf492360a1f220fe310f3be935acfb0347e
Omar Jabour <jabour.omar@hotmail.com>
2026-04-17 17:07:09 +0200

    :truck: Nest .  create mock.ts for cars  :truck:

---
+++ src/car/cars.mock.ts
@@ -0,0 +1,20 @@
+export const CARS = [
+    {
+        id: 1,
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2020
+    },
+    {
+        id: 2,
+        brand: 'Honda',
+        model: 'Civic',
+        year: 2019
+    },
+    {
+        id: 3,
+        brand: 'Ford',
+        model: 'Mustang',
+        year: 2021
+    }
+];
